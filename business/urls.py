38985934fab5e3434f857423d364d1e352149784
rupam office <rupamdhara.1579@gmail.com>
2025-11-28 18:12:53 +0530

    Updates';

--- business/urls.py
+++ business/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('categories/', CategoryListView.as_view(), name='categories'),
     path('select-location/', LocationSelectView.as_view(), name='location_select'),
     path('search/', SearchView.as_view(), name='search'),
-    path('category/<str:slug>', BusinessListView.as_view(), name='business_list'),
+    path('category/<str:slug>/', BusinessListView.as_view(), name='business_list'),
     path('<str:slug>/', BusinessDetailView.as_view(), name='business_detail'),
     
     
